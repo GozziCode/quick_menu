3651fef00adb2d851eb43064a0325c2b8b761c63
GozziCode <godwinekeuma@gmail.com>
2024-07-30 09:11:53 +0100

    update

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -7,45 +7,54 @@ class MenuScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Menu"),
-      backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        title: const Text("Menu"),
+        backgroundColor: Colors.transparent,
         surfaceTintColor: Colors.transparent,
       ),
       body: ListView.separated(
           scrollDirection: Axis.vertical,
           itemBuilder: (context, index) {
-            return MenuCategory();
-          }, separatorBuilder: (context, int){
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24),
+            return const MenuCategory();
+          },
+          separatorBuilder: (context, int) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 24),
               child: Divider(),
             );
-      }, itemCount: 3),
+          },
+          itemCount: 3),
     );
   }
 }
-
 
 class MenuCategory extends StatelessWidget {
   const MenuCategory({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-      padding: EdgeInsets.symmetric(horizontal: 24.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-
-        children: [
-          Text("Appetiser", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: AppColors.primaryColor),),
-
-          Column(crossAxisAlignment: CrossAxisAlignment.start,children: List.generate(3, (index){return MenuItem();}),),
-        ],
-      )
-    );
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Appetiser",
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.primaryColor),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: List.generate(3, (index) {
+                return const MenuItem();
+              }),
+            ),
+          ],
+        ));
   }
 }
-
 
 class MenuItem extends StatelessWidget {
   const MenuItem({super.key});
@@ -54,24 +63,37 @@ class MenuItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Row(
       children: [
-        
         Expanded(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 4.0),
-                child: Text("Cripsy Fried Chicken", style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),),
+                child: Text(
+                  "Cripsy Fried Chicken",
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                ),
               ),
-              Text("Crispy fried chicken is marinated, coated in a seasoned batter, and deep-fried", style: TextStyle(fontSize: 14.0,),),
+              Text(
+                "Crispy fried chicken is marinated, coated in a seasoned batter, and deep-fried",
+                style: TextStyle(
+                  fontSize: 14.0,
+                ),
+              ),
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 8.0),
-                child: Text("\$3.50", style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),),
+                child: Text(
+                  "\$3.50",
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                ),
               ),
-          
-            ],),
+            ],
+          ),
         ),
-        Icon(Icons.arrow_forward_ios_rounded, color: Colors.grey,)
+        Icon(
+          Icons.arrow_forward_ios_rounded,
+          color: Colors.grey,
+        )
       ],
     );
   }
